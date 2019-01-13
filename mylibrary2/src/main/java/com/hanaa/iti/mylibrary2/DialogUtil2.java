1d1cb36c25c46a6049e2fe0cc30d307bb679ada1
HanaaMAldaly <hanaaaldaly@gmail.com>
2019-01-14 00:55:56 +0200

    commit

--- mylibrary2/src/main/java/com/hanaa/iti/mylibrary2/DialogUtil2.java
+++ mylibrary2/src/main/java/com/hanaa/iti/mylibrary2/DialogUtil2.java
@@ -5,7 +5,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 
-public class DialogUtil {
+public class DialogUtil2 {
     public static void showDialog(Context context, String title, String message ,String positiveMsg, DialogInterface.OnClickListener listener){
         Dialog dialog=new AlertDialog.Builder(context)
                 .setTitle(title)
