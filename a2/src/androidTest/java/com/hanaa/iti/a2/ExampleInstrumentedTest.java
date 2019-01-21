692e72a84ce8a52ce54754dcf10377abd24b5717
HanaaMAldaly <hanaaaldaly@gmail.com>
2019-01-21 23:14:19 +0200

    commit20

---
+++ a2/src/androidTest/java/com/hanaa/iti/a2/ExampleInstrumentedTest.java
@@ -0,0 +1,16 @@
+package com.hanaa.iti.a2;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+public class ExampleInstrumentedTest {
+
+}
